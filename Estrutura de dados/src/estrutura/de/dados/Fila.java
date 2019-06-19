8f4315f1d40704fdb140dcc2fa1d36734cfabda2
eng-edu <eduardo.eng15@hotmail.com>
2019-06-19 18:51:39 -0300

    up

---
+++ Estrutura de dados/src/estrutura/de/dados/Fila.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estrutura.de.dados;
+
+/**
+lista define-se como uma sequência de elementos:
+onde cada elemento é caracterizado por uma estrutura com dois
+atributos:
+I um valor do elemento corrente (e.g. inteiro, objecto, etc), e
+I uma referência para elemento seguinte.
+e um conjunto de operações a realizar sobre a sequência:
+addFirst(val) inserir val no início da lista
+addLast(val) inserir val no fim da lista
+add(val, index) inserir val na posição index
+removeFirst() remover o primeiro elemento
+remove(index) remover o elemento na posição index
+removeLast() remover o último elemento
+get(index) retornar o elemento na posição index
+indexOf(val) retorna a posição da 1a ocorrência de val
+empty() verificar se a lista está vazia
+
+ */
+public class Fila {
+    
+}
--- Estrutura de dados/src/estrutura/de/dados/Pilha.java
+++ Estrutura de dados/src/estrutura/de/dados/Pilha.java
@@ -1,79 +1,73 @@
 package estrutura.de.dados;
-
+ 
 public class Pilha {
 
     //A pilha é uma das estruturas de dados e trabalha com o formato LIFO (o último a entrar é o primeiro a sair, “Last In, First Out”, em inglês).
     
     private int capacidade; //capacidade maxima da pilha
     private Object pilha[]; //pilha em formato de vetor de objetos
-    private int top;//grava o quantidade elementos que existe na pilha
+    private int topo;//grava o quantidade elementos que existe na pilha
  
 
     //contrutor para inicializar a pilha
     public Pilha(int size) {
         this.capacidade = size; 
         this.pilha = new Object[size];
-        this.top = 0;
+        this.topo = 0;
        
     }
 
    
     //verifica se esta vazia
-    public boolean isEmpty(){return top == 0;}
+    public boolean estaVazia(){return topo == 0;}
     
     //verifica se estar cheia
-    public boolean isFull(){return capacidade == top;}
+    public boolean estaCheia(){return capacidade == topo;}
     
     
     //insere no topo da pilha
-    public void push(Object object){
-        if(isFull()){
+    public void insereNoTopo(Object object){
+        if(estaCheia()){
             System.out.println("Pilha cheia");
         }else{
-            this.pilha[top] = object;
-            top++;
+            this.pilha[topo] = object;
+            topo++;
             
         }
     }
     
     //observa o top da 
-    public Object top(){
-        if(isEmpty()){
+    public Object verTopo(){
+        if(estaVazia()){
             System.out.println("Pilha vazia");
         }else{
-           return this.pilha[top-1];
+           return this.pilha[topo-1];
         }
         return null;
     }
     
     
      //remove do topo da pilha
-    public void pop(){
+    public void removeDoTopo(){
         
-        if(isEmpty()){
+        if(estaVazia()){
            System.out.println("Pilha vazia");  
         }else{
-            top--;
-            this.pilha[top] = null;
+            topo--;
+            this.pilha[topo] = null;
         }
     }
     
-    
-    //faz im print da pilh;
-    public void print(){
-        for(int i= capacidade-1; i >= 0; i--){
-            System.out.println(this.pilha[i]);
-        }
-    }
+
 
     //main
     public static void main(String[] args) {
         
         Pilha p = new Pilha(11);
-        p.push("eduardo lima");
-        p.push("elane lima");
-        p.pop();
-        p.print(); //resultado só o eduardo lima está na pilha
+        p.insereNoTopo("eduardo lima");
+        p.insereNoTopo("elane lima");
+        p.removeDoTopo();
+        System.out.println(p.verTopo());
     }
     
 };
